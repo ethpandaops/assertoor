e69fd6699405df02ef3b4989861d20e49a626c11
pk910 <philipp@pk910.de>
2024-01-03 22:54:10 +0100

    added `check_consensus_forks` task

---
+++ pkg/coordinator/tasks/check_consensus_forks/config.go
@@ -0,0 +1,18 @@
+package checkconsensusforks
+
+type Config struct {
+	MinCheckEpochCount uint64 `yaml:"minCheckEpochCount" json:"minCheckEpochCount"`
+	MaxForkDistance    uint64 `yaml:"maxForkDistance" json:"maxForkDistance"`
+	MaxForkCount       uint64 `yaml:"maxForkCount" json:"maxForkCount"`
+}
+
+func DefaultConfig() Config {
+	return Config{
+		MinCheckEpochCount: 1,
+		MaxForkDistance:    1,
+	}
+}
+
+func (c *Config) Validate() error {
+	return nil
+}
