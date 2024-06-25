09cf98a4a239bf63c88ad2138aa11d2f5973423b
pk910 <philipp@pk910.de>
2024-06-25 21:38:39 +0200

    add some standard utilities to docker image

--- pkg/coordinator/tasks/check_clients_are_healthy/config.go
+++ pkg/coordinator/tasks/check_clients_are_healthy/config.go
@@ -7,14 +7,16 @@ import (
 )
 
 type Config struct {
-	ClientPattern      string         `yaml:"clientPattern" json:"clientPattern"`
-	PollInterval       human.Duration `yaml:"pollInterval" json:"pollInterval"`
-	SkipConsensusCheck bool           `yaml:"skipConsensusCheck" json:"skipConsensusCheck"`
-	SkipExecutionCheck bool           `yaml:"skipExecutionCheck" json:"skipExecutionCheck"`
-	ExpectUnhealthy    bool           `yaml:"expectUnhealthy" json:"expectUnhealthy"`
-	MinClientCount     int            `yaml:"minClientCount" json:"minClientCount"`
-	MaxUnhealthyCount  int            `yaml:"maxUnhealthyCount" json:"maxUnhealthyCount"`
-	FailOnCheckMiss    bool           `yaml:"failOnCheckMiss" json:"failOnCheckMiss"`
+	ClientPattern         string         `yaml:"clientPattern" json:"clientPattern"`
+	PollInterval          human.Duration `yaml:"pollInterval" json:"pollInterval"`
+	SkipConsensusCheck    bool           `yaml:"skipConsensusCheck" json:"skipConsensusCheck"`
+	SkipExecutionCheck    bool           `yaml:"skipExecutionCheck" json:"skipExecutionCheck"`
+	ExpectUnhealthy       bool           `yaml:"expectUnhealthy" json:"expectUnhealthy"`
+	MinClientCount        int            `yaml:"minClientCount" json:"minClientCount"`
+	MaxUnhealthyCount     int            `yaml:"maxUnhealthyCount" json:"maxUnhealthyCount"`
+	FailOnCheckMiss       bool           `yaml:"failOnCheckMiss" json:"failOnCheckMiss"`
+	ExecutionRPCResultVar string         `yaml:"executionRpcResultVar" json:"executionRpcResultVar"`
+	ConsensusRPCResultVar string         `yaml:"consensusRpcResultVar" json:"consensusRpcResultVar"`
 }
 
 func DefaultConfig() Config {
