11321ed5bda1692bd1ba1da88cccced752fb006e
tosettil-polimi <info@tosettil.me>
2025-06-11 15:24:17 +0200

    Refactor tx_pool_latency_analysis configuration to include QPS parameter and reorder fields for improved clarity

--- pkg/coordinator/tasks/tx_pool_latency_analysis/config.go
+++ pkg/coordinator/tasks/tx_pool_latency_analysis/config.go
@@ -3,18 +3,20 @@ package txpoollatencyanalysis
 type Config struct {
 	PrivateKey string `yaml:"privateKey" json:"privateKey"`
 
-	TxCount							 int   `yaml:"txCount" json:"txCount"`
+	QPS                  int   `yaml:"qps" json:"qps"`
+	TxCount              int   `yaml:"txCount" json:"txCount"`
 	MeasureInterval      int   `yaml:"measureInterval" json:"measureInterval"`
-	HighLatency		       int64 `yaml:"highLatency" json:"highLatency"`
+	HighLatency          int64 `yaml:"highLatency" json:"highLatency"`
 	FailOnHighLatency    bool  `yaml:"failOnHighLatency" json:"failOnHighLatency"`
 	SecondsBeforeRunning int64 `yaml:"secondsBeforeRunning" json:"secondsBeforeRunning"`
 }
 
 func DefaultConfig() Config {
 	return Config{
-		TxCount: 							1000,
+		QPS:                  1000,
+		TxCount:              1000,
 		MeasureInterval:      100,
-		HighLatency:					5000, // in microseconds
+		HighLatency:          5000, // in microseconds
 		FailOnHighLatency:    true,
 		SecondsBeforeRunning: 0,
 	}
