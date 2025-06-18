c33f1bd604aef0e6a3bc59d20dcebf8ec543defa
tosettil-polimi <info@tosettil.me>
2025-06-18 11:50:12 +0200

    fix(lint-go): update golangci-lint command to use local binary for consistency in linting execution

--- scripts/lint-go.sh
+++ scripts/lint-go.sh
@@ -10,7 +10,7 @@ fi
 
 # Run golangci-lint to fix any issues automatically where applicable
 echo "Running golangci-lint to apply automatic fixes..."
-golangci-lint run --fix
+./bin/golangci-lint run --fix
 
 # Now run staticcheck to analyze the code
 echo "Running staticcheck..."
